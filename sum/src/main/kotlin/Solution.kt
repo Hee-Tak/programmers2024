08913f05c6169a0a7f611a1f2b748fe8b80063dd
hitak <hitak77@naver.com>
2024-01-11 18:04:14 +0900

    level 0) 두 수의 합

---
+++ sum/src/main/kotlin/Solution.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        return num1+num2
+    }
+}
+
+/**
+ * level 0) 두 수의 합
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120802?language=kotlin
+ */
