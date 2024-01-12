c3ff41b22d1a46b9cd33a85239e7c8e0aaafd00c
hitak <hitak77@naver.com>
2024-01-12 19:53:09 +0900

    level 0) a 와 b 출력하기

---
+++ print1/src/main/kotlin/Solution.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+    //println(a + b)
+    println("a = $a")
+    println("b = $b")
+}
+
+/**
+ * level 0) a 와 b 출력하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181951
+ */
